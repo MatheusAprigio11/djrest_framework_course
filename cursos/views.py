223492655bc36e245cc8f96b47cdb418ede724c9
Matheus Aprigio <matheusaprigio11>
2023-11-15 23:07:06 -0300

    updt views

--- cursos/views.py
+++ cursos/views.py
@@ -1,41 +1,28 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-
+from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
+class CursosAPIView(generics.ListCreateAPIView):
     """
-    API de Cursos
+    Listar e criar
     """
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"id":serializer.data['id'], "curso": serializer.data['titulo']}, status=status.HTTP_201_CREATED)
-    
-    
-class AvaliacaoAPIView(APIView):
-    """
-    API de Avaliações
-    """
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
 
-    
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Depende de um id para atualizar, destruir
+    """
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
